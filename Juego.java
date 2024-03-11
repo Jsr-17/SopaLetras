eee734baa34936dfd09aa8a99e5d0832270a9da9
Jsr017 <123515664+Jsr017@users.noreply.github.com>
2024-03-12 00:43:45 +0100

    Sopa dificil completada

--- Juego.java
+++ Juego.java
@@ -2,92 +2,179 @@ import java.util.Scanner;
 
 public class Juego extends Tablero {
 
-Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
-char abecedario[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char abecedario[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+            't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-@Override
-public void asignaValores(int dificultad) {
- if (dificultad==0) {
-    this.tableroJuego= new char[4][5];
-        for(int i=0;i<4;i++){
-            for(int j=0;j<5;j++){
-                if (i==0 && j==0 || i==0 && j==4 ||i==2 && j==1 ||i==3 && j==0 ) {
-                    tableroJuego[i][j]='A';
-                }else if(i==0 && j==2||i==2 && j==2||i==2 && j==3||i==3 && j==0){
-                    tableroJuego[i][j]='O';
-                }else if (i==0 && j==1) {
-                    tableroJuego[i][j]='H';
-                }else if (i==0 && j==3||i==3 && j==3|| i==3 && j==4) {
-                    tableroJuego[i][j]='L';                
-                }else if(i==1 && j==0 || i==1 && j==4){
-                    tableroJuego[i][j]='Z';
-                }else if(i==1 && j==1 || i==1 && j==3){
-                    tableroJuego[i][j]='S';
-                }else if (i==2 && j==0) {
-                    tableroJuego[i][j]='P';
-                }else if (i==3 && j==2 ) {
-                    tableroJuego[i][j]='C';
-                }else if (i==1 && j==2) {
-                    tableroJuego[i][j]='J';
-                }else if (i==3 && j==1) {
-                    tableroJuego[i][j]='R';
-                }else if (i==2 && j==4){
-                    tableroJuego[i][j]='U';
+    @Override
+    public void asignaValores(int dificultad) {
+        if (dificultad == 0) {
+            this.tableroJuego = new char[4][5];
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 5; j++) {
+                    if (i == 0 && j == 0 || i == 0 && j == 4 || i == 2 && j == 1 || i == 3 && j == 0) {
+                        tableroJuego[i][j] = 'A';
+                    } else if (i == 0 && j == 2 || i == 2 && j == 2 || i == 2 && j == 3 || i == 3 && j == 0) {
+                        tableroJuego[i][j] = 'O';
+                    } else if (i == 0 && j == 1) {
+                        tableroJuego[i][j] = 'H';
+                    } else if (i == 0 && j == 3 || i == 3 && j == 3 || i == 3 && j == 4) {
+                        tableroJuego[i][j] = 'L';
+                    } else if (i == 1 && j == 0 || i == 1 && j == 4) {
+                        tableroJuego[i][j] = 'Z';
+                    } else if (i == 1 && j == 1 || i == 1 && j == 3) {
+                        tableroJuego[i][j] = 'S';
+                    } else if (i == 2 && j == 0) {
+                        tableroJuego[i][j] = 'P';
+                    } else if (i == 3 && j == 2) {
+                        tableroJuego[i][j] = 'C';
+                    } else if (i == 1 && j == 2) {
+                        tableroJuego[i][j] = 'J';
+                    } else if (i == 3 && j == 1) {
+                        tableroJuego[i][j] = 'R';
+                    } else if (i == 2 && j == 4) {
+                        tableroJuego[i][j] = 'U';
+                    }
+
                 }
+            }
 
+        } else {
+            this.tableroJuego= new char[8][10];
+            for(int i =0;i<8;i++){
+                for(int j=0;j<10;j++){
+                    if (
+                        i==0 && j==0 || i==0 && j==6 || i==0 && j==8 || i==1 && j==4 ||
+                        i==1 && j==7 || i==1 && j==9 || i==2 && j==0 || i==2 && j==3 ||
+                        i==2 && j==7 || i==3 && j==1 || i==3 && j==2 || i==3 && j==3 ||
+                        i==3 && j==7 || i==4 && j==9 || i==5 && j==3 || i==6 && j==0 ||
+                        i==7 && j==4
+                    ) {
+                        tableroJuego[i][j]='O';
+                    }else if (
+                        i==0 && j==3 || i==2 && j==5 || i==4 && j==1 || i==6 && j==5|| i==3 && j==8
+                    ) {
+                        tableroJuego[i][j]='E';
+                    }else if (
+                        i==0 && j==1 || i==2 && j==1 || i==4 && j==4
+                    ) {
+                        tableroJuego[i][j]='D';
+                    }else if (
+                        i==0 && j==2 || i==0 && j==5 || i==2 && j==2 || i==2 && j==6 ||
+                        i==4 && j==2 || i==4 && j==3 || i==5 && j==7 || i==6 && j==7 ||
+                        i==7 && j==3 || i==6 && j==8
+                    ) {
+                        tableroJuego[i][j]='R';
+                    }else if (
+                        i==0 && j==4 || i==6 && j==1 
+                    ) {
+                        tableroJuego[i][j]='C';
+                    }else if (
+                        i==0 && j==9 || i==3 && j==0 || i==4 && j==5 || i==4 && j==6 ||
+                        i==5 && j==2 || i==6 && j==7 
+                    ) {
+                        tableroJuego[i][j]='X';
+                    }else if (
+                        i==0 && j==7 
+                    ) {
+                        tableroJuego[i][j]='J';
+                    }else if (
+                        i==0 && j==9 || i==3 && j==0 || i==4 && j==6 || i==4 && j==7 ||
+                        i==5 && j==2 || i==6 && j==8 
+                    ) {
+                        tableroJuego[i][j]='X';
+                    }else if (
+                        i==1 && j==0 || i==3 && j==9 || i==4 && j==8 ||i==7 && j==6
+                    ) {
+                        tableroJuego[i][j]='S';
+                    }else if (
+                        i==1 && j==3 || i==4 && j==0 || i==7 && j==0 
+                    ) {
+                        tableroJuego[i][j]='P';
+                    }else if (
+                        i==1 && j==1 || i==1 && j==2 || i==2 && j==9 || i==3 && j==5 ||
+                        i==5 && j==4 || i==5 && j==6 || i==5 && j==9 || i==6 && j==3 ||
+                        i==6 && j==9 || i==7 && j==7 || i==7 && j==1 || i==6 && j==6
+                    ) {
+                        tableroJuego[i][j]='A';
+                    }else if(
+                        i==1 && j==5 || i==1 && j==6 || i==3 && j==6 || i==3 && j==9 ||
+                        i==5 && j==0 || i==7 && j==8 || i==7 && j==9 
+                    ){
+                        tableroJuego[i][j]='L';
+                    }else if (
+                        i==1 && j==8 || i==5 && j==1 || i==5 && j==1 || i==6 && j==4 ||
+                        i==7 && j==2 
+                    ) {
+                        tableroJuego[i][j]='T';
+
+                    }else if (
+                        i==0 && j==4 || i==2 && j==8 || i==5 && j==8 || i==2 && j==4
+                    ) {
+                        tableroJuego[i][j]='M';
+                    }else if (
+                        i==7 && j==5 
+                    ) {
+                        tableroJuego[i][j]='N';
+                    }else if (
+                        i==6 && j==2 
+                    ) {
+                        tableroJuego[i][j]='H';
+                    }else if (  
+                        i==3 && j==4 || i==5 && j==5
+                    ) {
+                        tableroJuego[i][j]='I';
+                    }
+                      
+                }
             }
         }
-    
- }else{
 
- }
-    
-}
-    
-public void estableceDificultad(int dificultad){
-
-    switch (dificultad) {
-        case 0:
-        asignaValores(0);
-
-        break;
-       
-        case 1:
-            asignaValores(1);
-
-            break;
-        default:
-            break;
     }
-}
+
+    public void estableceDificultad(int dificultad) {
+
+        switch (dificultad) {
+            case 0:
+                asignaValores(0);
+
+                break;
+
+            case 1:
+                asignaValores(1);
+
+                break;
+            default:
+                break;
+        }
+    }
+
     @Override
     public void iniciaTablero() {
 
-        System.out.println("Elija el nivel de dificultad: \n 0=Normal  \n 1=Dificil ");
+        System.out.println("Elija el nivel de dificultad: \n -0=Normal  \n -1=Dificil ");
 
-        int dificultad=scanner.nextInt();
+        int dificultad = scanner.nextInt();
 
         estableceDificultad(dificultad);
 
-        if (dificultad==0) {
+        if (dificultad == 0) {
             for (int i = 0; i < 4; i++) {
                 for (int j = 0; j < 5; j++) {
-                    System.out.print(tableroJuego[i][j]+"  ");
+                    System.out.print(tableroJuego[i][j] + "  ");
                 }
                 System.out.println("");
-        }
-        }else{
+            }
+        } else {
             for (int i = 0; i < 8; i++) {
                 for (int j = 0; j < 10; j++) {
-                    System.out.print(tableroJuego[i][j]+"  ");
+                    System.out.print(tableroJuego[i][j] + "  ");
                 }
                 System.out.println("");
-        }
+            }
         }
 
-       
-        
     }
-    
+
 }
